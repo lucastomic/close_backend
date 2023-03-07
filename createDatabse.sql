aec08c952d4848698d24f3dd8aef161cd7cabe81
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-03-07 10:29:23 +0100

    Syntax fixed

--- createDatabse.sql
+++ createDatabse.sql
@@ -9,33 +9,33 @@ CREATE TABLE user(
   password VARCHAR(255), 
   
   PRIMARY KEY(phone)
-)        
+);
 
 CREATE TABLE photo(
   link VARCHAR(255),
-  userPhone VARCHAR,
+  userPhone VARCHAR(255),
   PRIMARY KEY(link),
   CONSTRAINT
-  FOREIGN KEY(userPhone) REFERENCES USER(phone)
+  FOREIGN KEY(userPhone) REFERENCES user(phone)
   ON DELETE CASCADE
   ON UPDATE CASCADE
-)
+);
 
 CREATE TABLE interest(
   name VARCHAR(255),
   PRIMARY KEY(name)
-)
+);
 
 CREATE TABLE userIsInterested(
   userPhone VARCHAR(255),
   interest VARCHAR(255),
   
   CONSTRAINT
-  FOREIGN KEY(userPhone) REFERENCES user(phone)
+  FOREIGN KEY(userPhone) REFERENCES user(phone),
   FOREIGN KEY(interest) REFERENCES interest(name)
   ON DELETE CASCADE
   ON UPDATE CASCADE
 
-)
+);
 
 
