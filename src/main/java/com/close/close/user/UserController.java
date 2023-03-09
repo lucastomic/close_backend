c9849ddb47cf1385812cd87afb6006e7867d294e
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-03-09 23:48:46 +0100

    controller added

---
+++ src/main/java/com/close/close/user/UserController.java
@@ -0,0 +1,13 @@
+package com.close.close.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * UserController is the controller from the User entity.
+ * It is in charge of handling the APIRest interactions with the User.
+ */
+
+@RestController
+public class UserController {
+}
