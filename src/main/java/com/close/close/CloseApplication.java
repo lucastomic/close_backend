cff1e88db8fecfd5fe6c8ac2ccc6a7555274afb8
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-03-09 22:09:44 +0100

    Initial structure

---
+++ src/main/java/com/close/close/CloseApplication.java
@@ -0,0 +1,13 @@
+package com.close.close;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloseApplication.class, args);
+	}
+
+}
