9219c441e21e585339c5a357e4dce9ebb63955d4
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-03-09 23:40:11 +0100

    doc added

--- src/main/java/com/close/close/user/UserRepository.java
+++ src/main/java/com/close/close/user/UserRepository.java
@@ -1,6 +1,10 @@
 package com.close.close.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * UserRepository is the user's implementation of the repository pattern.
+ * In charge of all the interactions with the DB.
+ */
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
