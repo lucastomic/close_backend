7d1d932d128799649b8d981c666de477f1b531c4
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-03-10 09:04:56 +0100

    getAll implemented

--- src/main/java/com/close/close/user/UserController.java
+++ src/main/java/com/close/close/user/UserController.java
@@ -1,7 +1,15 @@
 package com.close.close.user;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 
 /**
  * UserController is the controller from the User entity.
@@ -10,4 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    private UserRepository repository;;
+    private UserModelAssembler assembler;;
+    UserController(UserRepository repository, UserModelAssembler assembler){
+        this.repository = repository;
+        this.assembler = assembler;
+    };
+    @GetMapping("/users")
+    CollectionModel<EntityModel<User>> getAll(){
+            List<EntityModel<User>> users = repository.findAll().
+            stream().map(assembler::toModel).toList();
+
+            return CollectionModel.of(
+                    users,
+                    linkTo(methodOn(UserController.class).getAll()).withSelfRel()
+            );
+    }
 }
