d2c9f620d6c41894345d233878dfc1dc135f3252
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-03-10 09:15:09 +0100

    Doc added

--- src/main/java/com/close/close/user/UserController.java
+++ src/main/java/com/close/close/user/UserController.java
@@ -15,15 +15,33 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
  * UserController is the controller from the User entity.
  * It is in charge of handling the APIRest interactions with the User.
  */
-
 @RestController
 public class UserController {
+    /**
+     * repository is the user's repository for DB interaction
+    */
     private UserRepository repository;;
+    /**
+     * assembler is the user's model assembler for converting User models to
+     * AIPRest responses
+     */
     private UserModelAssembler assembler;;
+
+    /**
+     * Class constructor. It implements dependency injection.
+     * @param repository user's repository
+     * @param assembler user's model assembler
+     */
     UserController(UserRepository repository, UserModelAssembler assembler){
         this.repository = repository;
         this.assembler = assembler;
     };
+
+    /**
+     * getAll retrieves a CollectionModel with all the application's users.
+     * The response is linked to different methods of the APIRest.
+     * @return CollectionModel with different links of the APIRest.
+     */
     @GetMapping("/users")
     CollectionModel<EntityModel<User>> getAll(){
             List<EntityModel<User>> users = repository.findAll().
