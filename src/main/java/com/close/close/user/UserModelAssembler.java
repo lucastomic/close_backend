7fc89c719e708ef752f2fc29b1179b96e0b7165f
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-03-10 09:05:14 +0100

    Implemented. Need to complete

--- src/main/java/com/close/close/user/UserModelAssembler.java
+++ src/main/java/com/close/close/user/UserModelAssembler.java
@@ -4,6 +4,8 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 
 /**
@@ -11,9 +13,14 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class UserModelAssembler  implements RepresentationModelAssembler<User, EntityModel<User>> {
-    //TODO:IMPLEMENT
+    //TODO:COMPLETE
     @Override
     public EntityModel<User> toModel(User user) {
-        return EntityModel.of(user);
+        return EntityModel.of(
+                user,
+                linkTo(methodOn(UserController.class).getAll()).withRel("user")
+
+        );
     }
+
 }
