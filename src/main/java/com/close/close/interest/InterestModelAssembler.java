5d7d942bd3a45a32ec9ff71645c66a70744d8190
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-03-15 18:00:41 +0100

    InterestModelAssembler completed

--- src/main/java/com/close/close/interest/InterestModelAssembler.java
+++ src/main/java/com/close/close/interest/InterestModelAssembler.java
@@ -1,4 +1,24 @@
 package com.close.close.interest;
 
-public class InterestModelAssembler {
+import com.close.close.user.User;
+import com.close.close.user.UserController;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+/**
+ * InterestModelAssembler is in charge of modeling the User entity for APIRest responses.
+ */
+public class InterestModelAssembler implements RepresentationModelAssembler<Interest, EntityModel<Interest>> {
+
+    @Override
+    public EntityModel<Interest> toModel(Interest interest) {
+        return EntityModel.of(
+                interest,
+                linkTo(methodOn(InterestController.class).getAll()).withRel("users"),
+                linkTo(methodOn(InterestController.class).getOne(interest.getName())).withSelfRel()
+        );
+    }
 }
