2a1f243db76ba11eafe405917cc505bec9e0e004
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-03-15 13:11:07 +0100

    doc added

--- src/main/java/com/close/close/interest/Interest.java
+++ src/main/java/com/close/close/interest/Interest.java
@@ -7,10 +7,21 @@ import jakarta.persistence.ManyToMany;
 import java.util.Set;
 
 
+/**
+ * Interest are the user's interests
+ */
 @Entity
 public class Interest {
+    /**
+     * Interest's name (for example "futbol", "videogames", etc).
+     * It's the entity id
+     */
     @Id
     private String name;
+    /**
+     * userInterested is a collection with the users who
+     * have this interest.
+     */
     @ManyToMany(mappedBy = "interests")
     private Set<User> usersInterested;
 }
