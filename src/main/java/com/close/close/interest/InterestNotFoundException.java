f549d03fe83bf9fe1e8c1b25780198949bf5c08e
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-03-15 18:01:18 +0100

    InterestNotFoundException completed

--- src/main/java/com/close/close/interest/InterestNotFoundException.java
+++ src/main/java/com/close/close/interest/InterestNotFoundException.java
@@ -1,4 +1,10 @@
 package com.close.close.interest;
 
-public class InterestNotFoundException {
+/**
+ * InterestNotFoundException indicates that no interest have been found with the specifications given
+ */
+public class InterestNotFoundException extends RuntimeException{
+    InterestNotFoundException(){
+        super("No interest found");
+    }
 }
