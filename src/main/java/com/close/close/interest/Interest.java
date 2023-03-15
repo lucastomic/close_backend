7be64de9deed301ea6e0856e8894ffaacc90eeea
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-03-15 13:02:01 +0100

    annotations added

--- src/main/java/com/close/close/interest/Interest.java
+++ src/main/java/com/close/close/interest/Interest.java
@@ -1,11 +1,16 @@
 package com.close.close.interest;
 import com.close.close.user.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 
 import java.util.Set;
 
 
+@Entity
 public class Interest {
+    @Id
+    private String name;
     @ManyToMany(mappedBy = "interests")
     private Set<User> usersInterested;
 }
