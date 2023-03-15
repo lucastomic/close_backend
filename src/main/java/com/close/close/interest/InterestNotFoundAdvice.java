b04b065e2e11028966044a3a95ec38349cbc8246
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-03-15 18:03:12 +0100

    doc added

--- src/main/java/com/close/close/interest/InterestNotFoundAdvice.java
+++ src/main/java/com/close/close/interest/InterestNotFoundAdvice.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * InterestNotFoundAdvice handles the InterestNotFound exceptions
+ */
 @ControllerAdvice
 public class InterestNotFoundAdvice {
     /**
