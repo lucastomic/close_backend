1515a3e2e8be117d2f7805c7d9b4201319a6cc92
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-03-15 18:00:57 +0100

    InterestNotFoundAdvice completed

--- src/main/java/com/close/close/interest/InterestNotFoundAdvice.java
+++ src/main/java/com/close/close/interest/InterestNotFoundAdvice.java
@@ -1,4 +1,23 @@
 package com.close.close.interest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class InterestNotFoundAdvice {
+    /**
+     * exceptionHandler is triggered every time a InterestNotFoundException is
+     * thrown. It returns the exception message with a 404 (not found) status code.
+     * @param ex exception throwed
+     * @return exception message with 404 status
+     */
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(InterestNotFoundException.class)
+    String exceptionHandler(InterestNotFoundException ex){
+        return ex.getMessage();
+    }
 }
