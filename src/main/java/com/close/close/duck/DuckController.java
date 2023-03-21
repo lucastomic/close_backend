e4f5b0797d969b37c8e8763cce150358c567a1dc
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-03-21 11:27:42 +0100

    Doc added

--- src/main/java/com/close/close/duck/DuckController.java
+++ src/main/java/com/close/close/duck/DuckController.java
@@ -1,6 +1,5 @@
 package com.close.close.duck;
 
-
 import com.close.close.apirest.UserUtils;
 import com.close.close.user.User;
 import com.close.close.user.UserModelAssembler;
@@ -8,23 +7,37 @@ import com.close.close.user.UserRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
-import jakarta.persistence.TypedQuery;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Arrays;
 import java.util.List;
 
+/**
+ * DuckController is the controller for all duck's logic interactions
+ */
 @RestController
 public class DuckController {
+    /**
+     * repository is the duck's implementations of the repository pattern.
+     * Handles all the duck's DB interactions
+     */
     private DuckRepository repository;
+    /**
+     * userRepository is the user's implementation of the repository pattern.
+     * Handles all the user's DB interactions
+     */
     private UserRepository userRepository;
+    /**
+     * userModelAssmebler manages the User entity modeling
+     */
     private UserModelAssembler userModelAssembler;
     @PersistenceContext
     private EntityManager entityManager;
 
+    /**
+     * class constructor with dependency injection
+     */
     DuckController(DuckRepository repository, UserRepository userRepository, UserModelAssembler userModelAssembler){
         this.userModelAssembler = userModelAssembler;
         this.userRepository = userRepository;
@@ -50,6 +63,12 @@ public class DuckController {
         return ResponseEntity.ok().body(body);
     }
 
+    /**
+     * getDucksReceived return a collection of the users who have sent a
+     * duck to the user with the specified ID.
+     * @param id id of the user to know who have sent a duck to
+     * @return CollectionModel with all the users who have sent a duck to this user
+     */
     @GetMapping("/ducksReceived/{id}")
     CollectionModel<EntityModel<User>> getDucksReceived(@PathVariable Long id){
        String queryString = "SELECT d.sender FROM Duck d WHERE d.receiver.id = :id";
