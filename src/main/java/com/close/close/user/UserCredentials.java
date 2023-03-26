bb511a26d584b483f8c8acc07f8f6433568ac622
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-03-26 06:24:12 +0200

    Login added (Need to finish)

---
+++ src/main/java/com/close/close/user/UserCredentials.java
@@ -0,0 +1,22 @@
+package com.close.close.user;
+
+public class UserCredentials {
+    private String phone;
+    private String password;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setUserName(String userName) {
+        this.phone = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
