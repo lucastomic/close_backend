00950d40b92d12f37fecbcae6edef03ca2070e87
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-03-30 10:42:51 +0200

    Rename

--- src/main/java/com/close/close/user/UserRepository.java
+++ src/main/java/com/close/close/user/UserRepository.java
@@ -3,6 +3,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 /**
  * UserRepository is the user's implementation of the repository pattern.
  * In charge of all the interactions with the DB.
@@ -10,10 +12,10 @@ import org.springframework.data.repository.query.Param;
 public interface UserRepository extends JpaRepository<User, Long>{
 
     /**
-     * findByPhone gets the user given a number phone
+     * getUserFromPhone gets the user given a number phone
      * @param phone phone of the user to look for
      * @return user with that phone
      */
     @Query("SELECT u FROM User u WHERE u.phone = :phone")
-    public User findByPhone(@Param("phone") String phone);
+    public User getUserFromPhone(@Param("phone") String phone);
 }
