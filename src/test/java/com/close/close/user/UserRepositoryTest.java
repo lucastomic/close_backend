688ff110fe543d7593005f0bc4bcab320aaf5273
AndreesBenitoo <a.benito.llorente@gmail.com>
2023-04-04 11:37:13 +0200

    UserRepositoryTest doesnt work yet. just little upgrade

--- src/test/java/com/close/close/user/UserRepositoryTest.java
+++ src/test/java/com/close/close/user/UserRepositoryTest.java
@@ -1,27 +1,66 @@
 package com.close.close.user;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
 
-class UserRepositoryTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class UserRepositoryTest {
+
     @Autowired
-    private UserRepository underTest;
-    @Test
-    void itShouldCheckIfUserFindByPhone() {
-        // given
+    private TestEntityManager entityManager;
 
-        String phoneNumber = "+34 663796810";
-        User user = new User(
-                1L,"AndreesBenito","AndresBenito",19,"1234",Role.USER, phoneNumber,true
-        );
-        underTest.save(user);
-        // when
+    @Autowired
+    private UserRepository userRepository;
 
-        // then
+    private User testUser;
+
+    @BeforeEach
+    public void setUp() {
+        testUser = new User(
+                1L,"testUsername","profileName",19,
+                "1234",Role.USER,"123456789",false);
+        entityManager.persist(testUser);
+        entityManager.flush();
     }
 
     @Test
-    void itShouldSelectFindByUsername() {
+    public void findByPhone_userExists_returnsUser() {
+        Optional<User> foundUser = userRepository.findByPhone(testUser.getPhone());
+
+        assertThat(foundUser.isPresent()).isTrue();
+        assertThat(foundUser.get().getPhone()).isEqualTo(testUser.getPhone());
+    }
+
+    @Test
+    public void findByPhone_userNotExists_returnsEmptyOptional() {
+        Optional<User> foundUser = userRepository.findByPhone("987654321");
+
+        assertThat(foundUser.isPresent()).isFalse();
+    }
+
+    @Test
+    public void findByUsername_userExists_returnsUser() {
+        Optional<User> foundUser = userRepository.findByUsername(testUser.getUsername());
+
+        assertThat(foundUser.isPresent()).isTrue();
+        assertThat(foundUser.get().getUsername()).isEqualTo(testUser.getUsername());
+    }
+
+    @Test
+    public void findByUsername_userNotExists_returnsEmptyOptional() {
+        Optional<User> foundUser = userRepository.findByUsername("nonexistentUsername");
+
+        assertThat(foundUser.isPresent()).isFalse();
     }
 }
+
