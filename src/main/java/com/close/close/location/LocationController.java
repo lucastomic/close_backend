b2e0907356130398731f501c7580341e05b8fb3c
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-08-25 20:02:08 +0200

    Real time user updating problem (unsolved)

--- src/main/java/com/close/close/location/LocationController.java
+++ src/main/java/com/close/close/location/LocationController.java
@@ -1,13 +1,11 @@
 package com.close.close.location;
 
-import com.close.close.security.AuthenticationService;
 import com.close.close.user.User;
+import com.close.close.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
 
 @RestController
 public class LocationController {
@@ -15,12 +13,12 @@ public class LocationController {
     public static final String USER_LOCATION = "/location";
 
     private final LocationService LOCATION_SERVICE;
-    private final AuthenticationService AUTH_SERVICE;
+    private final UserService USER_SERVICE;
 
     @Autowired
-    public LocationController(LocationService locationService, AuthenticationService authenticationService) {
+    public LocationController(LocationService locationService, UserService userService) {
         LOCATION_SERVICE = locationService;
-        AUTH_SERVICE = authenticationService;
+        USER_SERVICE = userService;
     }
 
     /**
@@ -31,6 +29,12 @@ public class LocationController {
     @MessageMapping(USER_LOCATION)
     public void sendLocation(@RequestBody Location location, Authentication authentication) {
         User user = (User)authentication.getPrincipal();
+        // As the Authentication object is set up when the user establishes the WebSocket connection,
+        // the object won't be updated until they make another WebSocket connection. Even if the user changes their
+        // info (adds an interest, removes a social network, etc.). So, for real time info updating, the
+        // Authentication object must be updated from the repository, for example, with:
+
+        // user = USER_SERVICE.findById(user.getId());
         LOCATION_SERVICE.sendUserLocation(user, location);
     }
 }
