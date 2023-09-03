3f0576301cc057798787d69798e0815806e3d0be
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-09-03 21:33:49 +0200

    Bug solved

--- src/main/java/com/close/close/location/LocationSenderService.java
+++ src/main/java/com/close/close/location/LocationSenderService.java
@@ -3,6 +3,7 @@ package com.close.close.location;
 import com.close.close.location.space_partitioning.QueryResult;
 import com.close.close.user.User;
 import com.close.close.user.UserNotFoundException;
+import com.close.close.user.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -17,7 +18,7 @@ public class LocationSenderService {
     private final SimpMessagingTemplate simpMessagingTemplate;
     private final LocationService LOCATION_SERVICE;
     public static final String WS_MESSAGE_TRANSFER_DESTINATION = "/queue/closeusers";
-    private final ArrayList<User> receivers ; //TODO: When a user hasn't send it's location for X time, should be deleted from receivers
+    private final ArrayList<User> receivers; //TODO: When a user hasn't send it's location for X time, should be deleted from receivers
     private User currentReceiver;
 
     @Autowired
@@ -27,39 +28,48 @@ public class LocationSenderService {
         receivers = new ArrayList<>();
     }
 
-    public void addReceiver(User user){
+    public void addReceiver(User user) {
         receivers.add(user);
     }
-    public void removeReceiver(User user){
+
+    public void removeReceiver(User user) {
         receivers.remove(user);
     }
+
     @Scheduled(fixedRate = 1000)
     public void sendMessages() {
-        for (User user: receivers){
+        for (User user : receivers) {
             currentReceiver = user;
             sendLocationToCurrentReceiver();
         }
     }
 
-    private void sendLocationToCurrentReceiver(){
-        Object responseBody = getResponseBody();
+    private void sendLocationToCurrentReceiver() {
+        List<User> responseBody = getResponseBody();
         String username = currentReceiver.getUsername();
-        simpMessagingTemplate.convertAndSendToUser(username,WS_MESSAGE_TRANSFER_DESTINATION, responseBody);
+        simpMessagingTemplate.convertAndSendToUser(username, WS_MESSAGE_TRANSFER_DESTINATION, parseToDTOList(responseBody));
     }
 
-    private Object getResponseBody(){
+    private List<User> getResponseBody() {
         try {
             return getResponseBodyWhenLocationWasSentFirst();
-        } catch (UserNotFoundException ex){
+        } catch (UserNotFoundException ex) {
             return getNoLocationSentFirstResponseBody();
         }
     }
 
-    private List<User> getResponseBodyWhenLocationWasSentFirst(){
+    private List<UserDTO> parseToDTOList(List<User> users){
+        List<UserDTO> response = users.stream().map(user -> {
+           return new UserDTO(user);
+        }).toList();
+        return response;
+    }
+
+    private List<User> getResponseBodyWhenLocationWasSentFirst() {
         return LOCATION_SERVICE.closeUsers(currentReceiver);
     }
 
-    private List<User>  getNoLocationSentFirstResponseBody(){
-        return  new ArrayList<>();
+    private List<User> getNoLocationSentFirstResponseBody() {
+        return new ArrayList<>();
     }
 }
