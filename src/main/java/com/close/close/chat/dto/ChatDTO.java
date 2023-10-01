fa8dc0f4dcc3619b056f715a5657621b367ce8c3
Lucas Tomic Benitez <lucas.tomic@slashmobility.com>
2023-10-01 19:34:07 +0200

    User to DTO

--- src/main/java/com/close/close/chat/dto/ChatDTO.java
+++ src/main/java/com/close/close/chat/dto/ChatDTO.java
@@ -4,22 +4,24 @@ import com.close.close.apirest.dto.DTO;
 import com.close.close.chat.Chat;
 import com.close.close.chat.Message;
 import com.close.close.user.User;
+import com.close.close.user.dto.UserDTO;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class ChatDTO extends DTO<Chat> implements Serializable {
     Long id;
-    Set<User> users;
+    Set<UserDTO> users;
     List<MessageDTO> messages;
 
     public ChatDTO(Chat chat) {
         super(chat);
     }
 
-    public Set<User> getUsers() {
+    public Set<UserDTO> getUsers() {
         return users;
     }
 
@@ -33,9 +35,18 @@ public class ChatDTO extends DTO<Chat> implements Serializable {
 
     @Override
     protected void initializeFields(){
-        users = (Set<User>) getPrivateField("users");
+        users = getUsersParsed();
         id = (Long) getPrivateField("id");
         messages = getMessagesParsed();
+    }
+
+    private Set<UserDTO> getUsersParsed(){
+        Set<User> rawUsers = (Set<User>) getPrivateField("users");
+        Set<UserDTO> response = new HashSet<>();
+        for (User user : rawUsers){
+            response.add(new UserDTO(user));
+        }
+        return response;
     }
 
     private List<MessageDTO> getMessagesParsed(){
