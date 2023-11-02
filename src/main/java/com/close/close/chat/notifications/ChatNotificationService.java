833f2e4ccdf3b382ab3225b1a3573d179b576158
lucastomic <ltomicb@gmail.com>
2023-11-02 22:18:00 +0100

    Notifications

--- src/main/java/com/close/close/chat/notifications/ChatNotificationService.java
+++ src/main/java/com/close/close/chat/notifications/ChatNotificationService.java
@@ -1,6 +1,9 @@
 package com.close.close.chat.notifications;
 
 import com.close.close.user.User;
+import com.close.close.user.dto.UserDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
@@ -18,15 +21,19 @@ public class ChatNotificationService implements IChatNotificationsService{
         try{
             Message msg = Message.builder()
                     .setToken(receiver.getNotificationDeviceID())
+                    .putData("user",getAsJSON(sender))
                     .setNotification(Notification.builder()
                             .setTitle("Some title")
                             .setBody("You have received a message from @" + sender.getUsername())
                             .build()).build();
-            String message = firebaseMessaging.send(msg);
-            System.out.println(message);
+            firebaseMessaging.send(msg);
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
+    }
+    private String getAsJSON(User sender) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(new UserDTO(sender));
     }
 }
 
